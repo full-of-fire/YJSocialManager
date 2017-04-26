c23872cf337177d1c46a6059e389dcb55ee6add1
full-of-fire <591730822@qq.com>
2017-04-26 10:58:37 +0800

    Initial commit

---
+++ Example/YJSocialManager/YJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YJAppDelegate.h
+//  YJSocialManager
+//
+//  Created by full-of-fire on 04/26/2017.
+//  Copyright (c) 2017 full-of-fire. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
